b0cf7eecb636c3c84641f8c727c17b0e7153d044
margaret manyeki <maggiemanyeki6@gmail.com>
2023-03-19 23:05:30 +0300

    added sln

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   get '/movies/:id/summary', to: 'movies#summary'
+    get '/movie_summaries', to: 'movies#summaries'
+
 
 end
